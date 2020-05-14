1cbcb3e920030c517c341a93ff2d36bba706685c
Sebastian Fabara <sebasfabara@knights.ucf.edu>
2020-05-14 15:12:38 -0400

    Original App Commit

---
+++ app.py
@@ -0,0 +1,10 @@
+import os
+from flask import Flask, render_template
+app = Flask(__name__, template_folder = 'templates')
+
+@app.route('/')
+def HOME():
+        return render_template('home.html')
+
+if __name__ == '__main__':
+    app.run()
