a678060d0487ecd11baa57d1479524e756f7cdc7
Copykat911 <65347650+Copykat911@users.noreply.github.com>
2020-05-21 07:58:15 -0400

    Update example.py

--- example.py
+++ example.py
@@ -1,1 +1,41 @@
-print("This is just a TEST")
+from flask import Flask, render_template, request, json
+from flaskext.mysql import MySQL
+app = Flask(__name__)
+app.debug = True
+
+mysql = MySQL()
+
+# MySQL configurations
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
+app.config['MYSQL_DATABASE_DB'] = 'BucketList'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+ooo=mysql.init_app(app)
+#conn = MySQL.connect(ooo)
+#cursor = conn.cursor(ooo)
+
+@app.route("/")
+@app.route("/main")
+def main():
+    return render_template('index.html')
+
+@app.route('/showSignUp')
+def showSignUp():
+    return render_template('signup.html')
+
+@app.route('/signUp', methods=['POST'])
+def signUp():
+ 
+    # read the posted values from the UI
+    _name = request.form['inputName']
+    _email = request.form['inputEmail']
+    _password = request.form['inputPassword']
+ 
+    # validate the received values
+    if _name and _email and _password:
+        return json.dumps({'html':'<span>All fields good !!</span>'})
+    else:
+        return json.dumps({'html':'<span>Enter the required fields</span>'})
+
+if __name__ == "__main__":
+    app.run()
