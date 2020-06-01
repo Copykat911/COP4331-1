e33166954b7c34177a276757baa195b962e97d18
Sebastian Fabara <sbs3128@live.com>
2020-06-01 04:12:33 -0400

    Delete Button

--- static/js/deleteUser.js
+++ static/js/deleteUser.js
@@ -2,9 +2,9 @@
 function deleteUser() {
 
     var list = document.getElementsByClassName("dangerb");
-    for (var i = 0; i < list.length; i++) 
+    for (var i = 0; i < list.length; i++)
     {
- 
+
      if(list[i].hasAttribute("style","display: none"))
      {
      list[i].setAttribute("style", "display: inline");
@@ -15,12 +15,23 @@ function deleteUser() {
      }
     }
 }
+function deleteIdea(){
+  var index, table = document.getElementsByClassName("theTable")
+  for(var i = 0; i < table.rows.length; i++)
+  {
+    table.rows[i].onclick = function()
+    {
+      index = this.parentElement.rowIndex;
+      console.log(index);
+    };
+  }
+}
 function closedeleteUser() {
 
   var list = document.getElementsByClassName("dangerb");
-  for (var i = 0; i < list.length; i++) 
+  for (var i = 0; i < list.length; i++)
   {
- 
+
    list[i].setAttribute("style", "display:none");
   }
 }
@@ -28,12 +39,18 @@ function closedeleteUser() {
 function editUser() {
 
   var list = document.getElementsByClassName("dangerc");
-  for (var i = 0; i < list.length; i++) 
+  for (var i = 0; i < list.length; i++)
   {
     list[i].setAttribute("id", "butk" + i);
    list[i].setAttribute("style", "display:inline");
   }
 }
+
+
+
+
+
+
 // function deleteUser() {
 //   var x = document.getElementsByClassName("TB");
 //   console.log(x)
@@ -43,4 +60,4 @@ function editUser() {
 //     x.style = "display:none";
 //   }
 //   console.log(x)
-// } 
+// }
